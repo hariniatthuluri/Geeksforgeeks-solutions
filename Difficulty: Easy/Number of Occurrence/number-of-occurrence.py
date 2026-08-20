9341118ba2902aa6376018cf9fb56a608c081156
hariniatthuluri <hariniatthuluri@gmail.com>
2026-08-20 16:09:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of Occurrence/number-of-occurrence.py
+++ Difficulty: Easy/Number of Occurrence/number-of-occurrence.py
@@ -1,7 +1,30 @@
 class Solution:
     def countFreq(self, arr, target):
-        res=[]
-        for i in range(len(arr)):
-            if arr[i]==target:
-                res.append(i)
-        return (len(res))
+        n=len(arr)
+        low,high=0,len(arr)-1
+        first=-1
+        while low<=high:
+            mid=(low+high)//2
+            if target==arr[mid]:
+                first=mid
+                high=mid-1
+            elif target>arr[mid]:
+                low=mid+1
+            else:
+                high=mid-1
+                
+        if first==-1:
+            return 0
+        
+        low,high=0,n-1
+        last=-1
+        while low<=high:
+            mid=(low+high)//2
+            if target==arr[mid]:
+                last=mid
+                low=mid+1
+            elif target>arr[mid]:
+                low=mid+1
+            else:
+                high=mid-1
+        return last-first+1
